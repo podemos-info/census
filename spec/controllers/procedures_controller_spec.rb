a8267344e1a7b9a5fd4a9b261bc60ab16487a2c8
Leonardo Diez <leiodd@gmail.com>
2017-08-24 17:27:51 +0200

    Rubocop offense.

--- spec/controllers/procedures_controller_spec.rb
+++ spec/controllers/procedures_controller_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 describe ProceduresController, type: :controller do
   render_views
-  
+
   let(:resource_class) { Procedure }
   let(:all_resources) { ActiveAdmin.application.namespaces[:root].resources }
   let(:resource) { all_resources[resource_class] }
