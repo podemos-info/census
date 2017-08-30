a16fadf4befe5e6695a370e8d5d24efeb43b7793
Leonardo Diez <leiodd@gmail.com>
2017-08-30 17:04:14 +0200

    Document normalization tests fixed.

--- lib/census/normalizers/document.rb
+++ lib/census/normalizers/document.rb
@@ -2,14 +2,16 @@
 
 Normalizr.configure do
   add :document_dni do |value|
-    dni = value.upcase.gsub(/[\W\_]*/, "")
-    dni.rjust(dni[-1].match?(/\d/) ? 8 : 9, "0") if value.present?
+    if value.present?
+      dni = value.upcase.gsub(/[\W\_]*/, "")
+      dni.rjust(dni[-1].match?(/\d/) ? 8 : 9, "0") if value.present?
+    end
   end
 
   add :document_nie do |value|
     if value.present?
       nie = value.upcase.gsub(/[\W\_]*/, "")
-      nie[0] + nie[1..-1].rjust(8, "0") if nie.length > 2
+      nie[0] + nie[1..-1].rjust(nie[-1].match?(/\d/) ? 7 : 8, "0") if nie.length > 1
     end
   end
 
