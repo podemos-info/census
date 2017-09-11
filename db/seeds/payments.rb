881a8f9d9031ed9a3db16f4c5f0eb99094cc482a
Leonardo Diez <leiodd@gmail.com>
2017-09-11 18:32:54 +0200

    Credit card order processing completed. Rubocop offenses fixed.

--- db/seeds/payments.rb
+++ db/seeds/payments.rb
@@ -3,26 +3,26 @@
 require "census/faker/bank"
 
 def create_order(person, credit_card)
-  if credit_card
-    expires_at = Faker::Date.between(6.month.ago, 4.year.from_now)
-    payment_method = PaymentMethods::CreditCard.create!(
-      person: person,
-      name: "CC - #{person.last_name1}",
-      authorization_token: "PATATAT!",
-      expiration_year: expires_at.year,
-      expiration_month: expires_at.month,
-      payment_processor: :redsys
-    )
-  else
-    iban = Census::Faker::Bank.iban("ES")
-    payment_method = PaymentMethods::CreditCard.create!(
-      person: person,
-      name: "*" * 16 + iban[-4..-1],
-      iban: iban,
-      bic: Faker::Bank.swift_bic,
-      payment_processor: :sepa
-    )
-  end
+  payment_method = if credit_card
+                     expires_at = Faker::Date.between(6.month.ago, 4.year.from_now)
+                     PaymentMethods::CreditCard.create!(
+                       person: person,
+                       name: "CC - #{person.last_name1}",
+                       authorization_token: "PATATAT!",
+                       expiration_year: expires_at.year,
+                       expiration_month: expires_at.month,
+                       payment_processor: :redsys
+                     )
+                   else
+                     iban = Census::Faker::Bank.iban("ES")
+                     PaymentMethods::DirectDebit.create!(
+                       person: person,
+                       name: "*" * 16 + iban[-4..-1],
+                       iban: iban,
+                       bic: Faker::Bank.swift_bic,
+                       payment_processor: :sepa
+                     )
+                   end
 
   Order.create!(
     person: person,
@@ -42,14 +42,6 @@ end
   # create 10 direct credit card methods and orders
   orders2 = Person.verified.order("RANDOM()").limit(10).map do |person|
     create_order person, true
-
-    Order.create!(
-      person: person,
-      payment_method: payment_method,
-      currency: "EUR",
-      amount: Faker::Number.between(1, 10_000),
-      description: Faker::Lorem.sentence(1, true, 4)
-    )
   end
 
   # add orders to an order batch
