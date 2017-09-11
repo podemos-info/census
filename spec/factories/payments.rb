068af10b46126276e93e38f097b4dd529fbfe21a
Leonardo Diez <leiodd@gmail.com>
2017-09-11 17:47:21 +0200

    Factories refactored.

---
+++ spec/factories/payments.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require "census/faker/bank"
+
+FactoryGirl.define do
+  factory :credit_card, class: PaymentMethods::CreditCard do
+    transient do
+      expires_at { 4.years.from_now }
+    end
+
+    person
+    name { "CC - #{person.last_name1}" }
+    payment_processor { :redsys }
+    authorization_token { "PATATAT!" }
+    expiration_year { expires_at.year }
+    expiration_month { expires_at.month }
+
+    trait :expired do
+      expires_at { 2.month.ago }
+    end
+
+    trait :external do
+      return_url { "test" }
+      authorization_token { nil }
+      expiration_year { nil }
+      expiration_month { nil }
+    end
+  end
+
+  factory :direct_debit, class: PaymentMethods::DirectDebit do
+    name { "*" * 16 + iban[-4..-1] }
+    iban { Census::Faker::Bank.iban("ES") }
+    bic { Faker::Bank.swift_bic }
+    payment_processor { :sepa }
+  end
+
+  factory :order do
+    person
+    payment_method { FactoryGirl.create(:direct_debit, person: person) }
+
+    currency { "EUR" }
+    amount { Faker::Number.between(1, 10_000) }
+
+    description { Faker::Lorem.sentence(1, true, 4) }
+
+    trait :credit_card do
+      payment_method { FactoryGirl.build(:credit_card, person: person) }
+    end
+
+    trait :external do
+      payment_method { FactoryGirl.build(:credit_card, :external, person: person) }
+    end
+  end
+
+  factory :orders_batch do
+    description { Faker::Lorem.sentence(1, true, 4) }
+
+    after :build do |orders_batch|
+      orders_batch.orders = build_list(:order, 10, orders_batch: orders_batch) + build_list(:order, 10, :credit_card, orders_batch: orders_batch)
+    end
+  end
+end
---
+++ spec/factories/people.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require "census/faker/localized"
+require "census/faker/document_id"
+
+FactoryGirl.define do
+  sequence(:participa_id)
+
+  sequence(:scope_name) do |n|
+    "#{Faker::Lorem.sentence(1, true, 3)} #{n}"
+  end
+
+  sequence(:scope_code) do |n|
+    "#{Faker::Lorem.characters(4).upcase}-#{n}"
+  end
+
+  factory :scope_type do
+    name { Census::Faker::Localized.word }
+    plural { Census::Faker::Localized.literal(name.values.first.pluralize) }
+  end
+
+  factory :scope, aliases: [:address_scope, :document_scope] do
+    name { Census::Faker::Localized.literal(generate(:scope_name)) }
+    code { generate(:scope_code) }
+    scope_type
+
+    trait :local do
+      name { Census::Faker::Localized.literal("local") }
+      code { Settings.regional.local_code }
+      initialize_with { Scope.find_or_create_by(code: Settings.regional.local_code) }
+    end
+
+    factory :local_scope, traits: [:local]
+  end
+
+  factory :person do
+    transient do
+      foreign { Faker::Boolean.boolean(0.1) }
+      non_local { Faker::Boolean.boolean(0.1) }
+    end
+
+    first_name { Faker::Name.first_name }
+    last_name1 { Faker::Name.last_name }
+    last_name2 { Faker::Name.last_name }
+    born_at { Faker::Date.between(99.year.ago, 18.year.ago) }
+    gender { Person.genders.keys.sample }
+    address { Faker::Address.street_address }
+    postal_code { Faker::Address.zip_code }
+    email { Faker::Internet.unique.email }
+    phone { "0034" + Faker::Number.number(9) }
+    created_at { Faker::Time.between(3.years.ago, 3.day.ago, :all) }
+    extra { { participa_id: generate(:participa_id) } }
+
+    address_scope
+    document_scope
+
+    after :build do |person, evaluator|
+      foreign = evaluator.foreign
+      if person.document_type
+        foreign = person.document_type.to_sym != :dni
+      else
+        person.document_type = evaluator.foreign ? :dni : [:nie, :passport].sample
+        person.document_id = nil
+      end
+      person.document_id = Census::Faker::DocumentId.generate(person.document_type) unless person.document_id.present?
+
+      local_scope = create(:local_scope)
+      person.scope = create(:scope, parent: local_scope)
+      person.address_scope = person.scope unless evaluator.non_local
+      person.document_scope = local_scope unless foreign
+    end
+
+    trait :young do
+      born_at { Faker::Date.between(18.year.ago, 14.year.ago) }
+    end
+
+    trait :verified do
+      verified_by_document { true }
+    end
+  end
+
+  factory :download do
+    association :person, factory: :person, strategy: :build
+    expires_at { Faker::Date.between(1.day.from_now, 30.days.from_now) }
+  end
+end
