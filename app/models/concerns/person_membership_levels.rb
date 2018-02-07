10b3fca5a3820ecc04c5543cb7e79e29b6515568
Leonardo Diez <leiodd@gmail.com>
2018-02-07 17:46:56 +0100

    Simplified membership levels.

--- app/models/concerns/person_membership_levels.rb
+++ app/models/concerns/person_membership_levels.rb
@@ -18,15 +18,14 @@ module PersonMembershipLevels
 
     aasm column: :membership_level do
       state :person, initial: true
-      state :follower, :young_member, :member
+      state :follower, :member
 
       event :to_follower do
-        transitions from: [:person, :young_member, :member], to: :follower, guard: :verified?
+        transitions from: [:person, :member], to: :follower
       end
 
       event :to_member do
-        transitions from: [:person, :follower, :young_member], to: :member, guard: :memberable?
-        transitions from: [:person, :follower], to: :young_member, guard: :young_memberable?
+        transitions from: [:person, :follower], to: :member, guard: :memberable?
       end
     end
 
@@ -40,10 +39,6 @@ module PersonMembershipLevels
 
     def memberable?
       verified? && born_at < 18.years.ago
-    end
-
-    def young_memberable?
-      verified? && born_at > 18.years.ago
     end
 
     def verified?
