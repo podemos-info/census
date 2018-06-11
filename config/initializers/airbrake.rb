5bb368d9d774d6fe12264496dc456523c3970da7
Leonardo Diez <leiodd@gmail.com>
2018-06-11 18:34:46 +0200

    Add airbrake support (#199)

---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Airbrake.configure do |config|
+  config.environment = Rails.env
+  config.ignore_environments = %w(test)
+  config.host = Settings.system.airbrake.host
+  config.project_id = Settings.system.airbrake.project_id
+  config.project_key = Settings.system.airbrake.api_key
+end
