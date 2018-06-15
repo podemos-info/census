58fd39e13e98ba32d71822f87214c176d284cc7e
Leonardo Diez <leiodd@gmail.com>
2018-06-15 13:25:54 +0200

    Added mappings and metadata to scopes. Refactor seed as in participa2.

---
+++ db/migrate/20180615093511_add_metadata_to_scopes.rb
@@ -0,0 +1,6 @@
+class AddMetadataToScopes < ActiveRecord::Migration[5.2]
+  def change
+    add_column :scopes, :mappings, :jsonb, default: {}, null: false
+    add_column :scopes, :metadata, :jsonb, default: {}, null: false
+  end
+end
