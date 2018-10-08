baea826afc1cdd5619c9aa4a35c1c81e5535fd25
Leonardo Diez <leiodd@gmail.com>
2018-10-08 18:18:57 +0200

    Added an encrypted `sensible_data` attribute to payment methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_084428) do
+ActiveRecord::Schema.define(version: 2018_10_08_142956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -204,6 +204,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_084428) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "discarded_at"
+    t.string "encrypted_sensible_data"
     t.index ["information"], name: "index_payment_methods_on_information", using: :gin
     t.index ["person_id"], name: "index_payment_methods_on_person_id"
   end
