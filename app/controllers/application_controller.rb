aa11c9e7a4ec738a444eba48e3f33bcfdf815539
Leonardo Diez <leiodd@gmail.com>
2018-10-17 18:31:53 +0200

    Better locale handling (#219)
    * Set locale on all pages
    
    * Thread-safe locale switching
    
    See https://github.com/rails/rails/issues/34043

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,14 @@ class ApplicationController < ActionController::Base
 
   after_action :track_action
 
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+
+    I18n.with_locale(locale, &action)
+  end
+
   def edit; end
 
   def show; end
