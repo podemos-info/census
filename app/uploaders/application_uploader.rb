a7b06c86059dbff0c33d8cd1b090a02823ddb2e1
Leonardo Diez <leiodd@gmail.com>
2019-06-24 10:49:48 +0200

    Partition ids for uploads folders names

--- app/uploaders/application_uploader.rb
+++ app/uploaders/application_uploader.rb
@@ -4,6 +4,6 @@ class ApplicationUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "non-public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "non-public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{format("%09d", model.id).scan(/\d{3}/).join("/")}"
   end
 end
