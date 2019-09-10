8232f17042fd813e7ddc9656e32839d5ee34b22d
Leonardo Diez <leiodd@gmail.com>
2019-09-10 17:50:08 +0200

    Improved environments settings.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,5 +5,4 @@ require_relative "production"
 # Override settings to be used in staging environments
 Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
 end
