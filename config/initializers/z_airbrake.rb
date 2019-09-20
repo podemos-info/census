f3b6517b3c6d0ebc2923a2357dd0474ee06894e2
Leonardo Diez <leiodd@gmail.com>
2019-09-20 16:01:18 +0200

    Airbrake configuration fixed.

--- config/initializers/z_airbrake.rb
+++ config/initializers/z_airbrake.rb
@@ -6,4 +6,7 @@ Airbrake.configure do |config|
   config.host = Settings.system.airbrake.host
   config.project_id = Settings.system.airbrake.project_id
   config.project_key = Settings.system.airbrake.api_key
+  config.root_directory = Rails.root
+  config.logger = Rails.logger
+  config.blacklist_keys = Rails.application.config.filter_parameters
 end
