59d11804f24a0930d0133460243badf969aab20a
Leonardo Diez <leiodd@gmail.com>
2019-10-22 12:32:59 +0200

    Fix admin tracking on events

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -8,8 +8,13 @@ class Ahoy::Store < Ahoy::DatabaseStore
   def visit_model
     Visit
   end
+
+  def track_event(data)
+    data[:admin_id] = controller.current_admin.id if controller.current_admin
+    super(data)
+  end
 end
 
-Ahoy.user_method = ->(controller) { controller.current_admin }
+Ahoy.user_method = :current_admin
 Ahoy.server_side_visits = :when_needed
 Ahoy.geocode = false
