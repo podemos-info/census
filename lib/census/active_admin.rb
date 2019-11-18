49ae309713170a70404abfcac1a741d2a54aa8b7
Leonardo Diez <leiodd@gmail.com>
2019-11-18 18:36:49 +0100

    Fix for routing bug when using multiple belongs_to

--- lib/census/active_admin.rb
+++ lib/census/active_admin.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 require "census/active_admin/fast_filter"
+require "census/active_admin/multiple_belongs_to"
 require "census/active_admin/pages_fixes"
 require "census/active_admin/views/changes_attributes_table"
